67dac22e80c21f50878f7429ff629fc9f713e208
DANIIL GOLOTVIN <daniilgolotvin@yandex.ru>
2024-05-01 20:30:48 +0300

    new function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const initSlider = () => {
-	const slideButton = document.querySelectorAll(".slider-button .slider-wrapper")
+	const imageList = document.querySelector(".slider-wrapper .image-list");
+	const slideButton = document.querySelectorAll(" .slider-wrapper .slider-button ")
 
-	slideButton.forEach((button) => {
+	slideButton.forEach(button => {
 		button.addEventListener('click', () => {
 			const direction = button.id === "prev-slide" ? -1 : 1;
 			const scrollAmount = imageList.clientWidth * direction;
 			imageList.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-		})
-	})
+		});
+	}); 
 }
 
 
