7504f665a162f83a927ffc79e31c1e2f28452976
DANIIL GOLOTVIN <daniilgolotvin@yandex.ru>
2024-05-13 10:26:18 +0300

    final changes

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const initSlider = () => {
 	const imageList = document.querySelector('.slider-wrapper .image-list');
 	const slideButtons = document.querySelectorAll('.slider-wrapper .slide-button');
+	const sliderScrollbar = document.querySelector ('.container .slider-scrollbar');
+	const scrollbarThumb = sliderScrollbar.querySelector ('.scrollbar-thumb');
 	const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth; 
+
+	scrollbarThumb.addEventListener('mousedown', (e) => {
+		const startX = e.clientX;
+		const thumbPosition =  scrollbarThumb.offsetLeft; 
+
+		const handleMouseMove = (e) => {
+			const deltaX = e.clientX - startX;
+			const newThumbPosition = thumbPosition + deltaX;
+			const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+
+			const boundedPosition = Math.max(0, Math.min(maxThumbPosition,newThumbPosition));
+			const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+			scrollbarThumb.style.left = `${boundedPosition}px`;
+			imageList.scrollLeft = scrollPosition;
+		}
+		
+		const handleMouseUp = () => {
+			document.removeEventListener('mousemove', handleMouseMove);
+		document.removeEventListener('mouseup', handleMouseUp);
+		}
+
+		document.addEventListener('mousemove', handleMouseMove);
+		document.addEventListener('mouseup', handleMouseUp);
+	});
+
 
 	slideButtons.forEach((button) => {
 		button.addEventListener('click', () => {
@@ -15,8 +43,16 @@ const initSlider = () => {
 		slideButtons[1].style.display  = imageList.scrollLeft >= maxScrollLeft ? 'none' : 'block'; 
 	}
 
-	imageList.addEventListener('scroll', () => {
+	const updateScrollThumbPosition = () => {
+		const scrollPosition  =	imageList.scrollLeft;
+		const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.clientWidth);
+		scrollbarThumb.style.left = `${thumbPosition}px`;
+		
+	}
+
+	imageList.addEventListener("scroll", () => {
 		handleSliderButtons();
+		updateScrollThumbPosition();
 	})
 }
 
